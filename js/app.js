7e22c24c34c824c87f2aa5944e0c75b01b48556f
derekjanni <derekjanni@gmail.com>
2015-11-28 12:53:31 -0700

    working towards series/arts syntax

--- js/app.js
+++ js/app.js
@@ -7,8 +7,19 @@ app.config(function ($routeProvider) {
       templateUrl: 'views/home.html' 
     }) 
     .when('/photos/:id',{
+	    // Todo: edit to series/art framework
       controller: 'PhotoController',
       templateUrl: 'views/photo.html'
+  })
+     .when('/contact',{
+	     // Todo: make Contact controller
+       controller: 'ContactController',
+       templateUrl: 'contact.html'
+  })
+     .when('/about',{
+	     // Todo: make about controller
+      controller: 'AboutController',
+      templateUrl: 'contact.html'
   })
     .otherwise({ 
       redirectTo: '/' 
---
+++ js/services/series.js
@@ -0,0 +1,21 @@
+app.factory('photos', ['$http', function($http) {
+  return $http.get('http://www.joynelson.com/cockpit/rest/api/galleries/get/Series?token=25a01fb2eb534c8830882257') 
+          .success(function(data) {
+            // modify image path to usable url
+            for (i = 0; i < data.length; i++){ 
+              data[i]['data']['image'] = data[i]['data']['image'].replace('site:', 'http://joynelson.com/')
+            }
+            
+            // transform goofy JSON inputs into usable JSON
+            json_data = []
+            for (i = 0; i < data.length; i++){
+              json_data.push(data[i]['data'])
+            }
+
+	          return data;
+	         })
+          .error(function(data) {
+            return data;
+           });
+}]);
+
