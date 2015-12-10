c1445cd43fdff97a4610bbed1ca277f3e4135b04
derekjanni <derekjanni@gmail.com>
2015-12-09 17:33:29 -0700

    about page nearing completion, just needs css

--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -14,13 +14,14 @@ app.controller('HomeController', ['$scope', 'series', '$http', function($scope, 
             return $http.get('http://www.joynelson.com/cockpit/rest/api/collections/get/about?token=25a01fb2eb534c8830882257')
             .success(function(data) {
                 // modify image path to usable url
-                console.log(data[0]['about_text']);
-                $scope.about = data[0]['about_text'];
+                $scope.about = data[0];
             })
               .error(function(data) {
                 return data;
                });;
             }
         $scope.about = $scope.get_about();
+        console.log($scope.about);
+        //console.log($scope.about.image);
     });
 }]);
