2f9155a08975e1a6a5afa5fd90c2f2f50a94d58a
derekjanni <derekjanni@gmail.com>
2015-12-29 23:02:49 -0700

    contact form with nodemailer

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ app.config(function ($routeProvider, $locationProvider) {
       templateUrl: 'views/serie.html'
   })
      .when('/contact',{
-       controller: 'ContactController',
+       controller: 'HomeController',
        templateUrl: 'views/contact.html'
   })
      .when('/about',{
--- js/controllers/HomeController.js
+++ js/controllers/HomeController.js
@@ -1,5 +1,7 @@
 app.controller('HomeController', ['$scope', 'series', '$http', function($scope, series, $http) {
     series.success(function(data) {
+
+        // main series display
         $scope.series = data;
         $scope.seriesLength = $scope.series.length;
         $scope.feedLimit = 4;
@@ -10,6 +12,8 @@ app.controller('HomeController', ['$scope', 'series', '$http', function($scope, 
                 $scope.feedLimit -= 4;
             }
         };
+
+        // main about display
         $scope.get_about = function(){
             return $http.get('/api/about')
             .success(function(data) {
@@ -18,8 +22,23 @@ app.controller('HomeController', ['$scope', 'series', '$http', function($scope, 
             })
               .error(function(data) {
                 return data;
-               });;
+               });
             }
         $scope.about = $scope.get_about();
+
+        // contact form mail handler
+        $scope.sendMail = function(mail){
+            $http.post('api/contact', mail)
+            .success(function(data){
+                
+            })
+            .error(function(data){ console.log("error") });
+        }
+
+        // contact form reset
+        $scope.resetForm = function(){
+            $scope.model = '';
+            $scope.form.$setPristine(true);
+        }
     });
 }]);
